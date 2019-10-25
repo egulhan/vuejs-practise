1d49cf813629b1b8dce4d0fba4a3e84d4a7e261c
Erman Gülhan <mr.ermangulhan@gmail.com>
2019-10-26 01:04:16 +0300

    developed carousel component (flickity)

---
+++ vue-carousel-component/src/Carousel.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import Flickity from 'flickity';
+
+    export default {
+        name: "Carousel",
+        props: {
+            wraparound: {default: true},
+            autoplay: {default: false},
+            lazyload: {default: false},
+        },
+        data() {
+            return {
+                carouselClass: 'main-carousel',
+            };
+        },
+        mounted() {
+            new Flickity(this.$el, {
+                wraparound: this.wraparound,
+                autoPlay: this.autoplay,
+                lazyLoad: this.lazyload,
+            });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-carousel-component/src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Carousel from './Carousel';
+
+Vue.config.devtools = true;
+
+new Vue({
+    el:'#root',
+    components:{Carousel}
+});
